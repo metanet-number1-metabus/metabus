fe56d3f650a8d0c86bf607a0de9d65da91637d08
yjyj1023 <root@metanet.pri>
2023-05-17 13:22:36 +0900

    [Feat] Spring Boot 기본 설정

---
+++ src/main/java/com/metabus/metabus/MetabusApplication.java
@@ -0,0 +1,13 @@
+package com.metabus.metabus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetabusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetabusApplication.class, args);
+	}
+
+}
