60c94fae99baab21542e6da089502d212eed5f26
root <root@metanet.pri>
2023-05-26 17:47:27 +0900

    [Feat] chat 관련기능 백엔드 추가 (Socket관련 의존성 추가) METABUS-11

---
+++ src/main/java/com/metanet/metabus/chat/service/ChatService.java
@@ -0,0 +1,89 @@
+package com.metanet.metabus.chat.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import com.metanet.metabus.chat.dto.ChatDto;
+import com.metanet.metabus.chat.dto.RoomDto;
+import com.metanet.metabus.chat.entity.Chat;
+import com.metanet.metabus.chat.entity.Room;
+import com.metanet.metabus.chat.repository.ChatRepository;
+import com.metanet.metabus.chat.repository.RoomRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+    private final ObjectMapper objectMapper;
+    private final RoomRepository roomRepository;
+    private final ChatRepository chatRepository;
+
+    /**
+     * 모든 채팅방 찾기
+     */
+
+
+    public List<RoomDto> findAllRoom() {
+        List<Room> rooms = roomRepository.findAll();
+        return rooms.stream()
+                .map(RoomDto::of)
+                .collect(Collectors.toList());
+    }
+
+
+    /**
+     * 특정 채팅방 찾기
+     *
+     * @param id room_id
+     */
+
+
+    public RoomDto findRoomById(Long id) {
+        Optional<Room> RoomOptional = roomRepository.findById(id);
+        return RoomOptional.map(RoomDto::of).orElse(null);
+    }
+
+    /**
+     * 채팅방 만들기
+     *
+     * @param name 방 이름
+     */
+    public void createRoom(String name) {
+        roomRepository.save(Room.createRoom(name));
+    }
+
+    /////////////////
+
+
+    /**
+     * 채팅 생성
+     *
+     * @param roomId  채팅방 id
+     * @param sender  보낸이
+     * @param message 내용
+     */
+    public Chat createChat(Long roomId, String sender, String message) {
+        Room room = roomRepository.findById(roomId).orElseThrow();  //방 찾기 -> 없는 방일 경우 여기서 예외처리
+        return chatRepository.save(ChatDto.createChat(room, sender, message));
+    }
+
+
+    /**
+     * 채팅방 채팅내용 불러오기
+     *
+     * @param roomId 채팅방 id
+     */
+
+
+    public List<ChatDto> findAllChatByRoomId(Long roomId) {
+        List<Chat> chats = chatRepository.findAll();
+        return chats.stream()
+                .map(ChatDto::of)
+                .collect(Collectors.toList());
+    }
+
+
+}
