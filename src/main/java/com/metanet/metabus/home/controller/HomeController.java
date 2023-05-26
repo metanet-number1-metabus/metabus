3f2b17db66e957168f38913d8e88042ed85f5d07
yjyj1023 <lyj19961023@gmail.com>
2023-05-26 09:04:05 +0900

    [Update] 루트 경로 home으로 이동
    METABUS-5

--- src/main/java/com/metanet/metabus/home/controller/HomeController.java
+++ src/main/java/com/metanet/metabus/home/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.metanet.metabus.home.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
     @GetMapping(value = "")
     public String index() {
-        return "main/index";
+        return "/main/index";
     }
 }
