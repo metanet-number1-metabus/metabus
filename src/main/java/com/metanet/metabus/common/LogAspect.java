f3da5f4325b8de6da9ee2522afd98016521ba332
yjyj1023 <lyj19961023@gmail.com>
2023-05-31 11:39:39 +0900

    [Feat] AOP를 이용한 메서드 접근 로그기능 추가
    METABUS-10

---
+++ src/main/java/com/metanet/metabus/common/LogAspect.java
@@ -0,0 +1,19 @@
+package com.metanet.metabus.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class LogAspect {
+    @Before("execution(* com.metanet.metabus..*(..))")
+    public void logMethodAccess(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getName();
+        System.out.println("Accessing method: " + className + "." + methodName);
+    }
+}
