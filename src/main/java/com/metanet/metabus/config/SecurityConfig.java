94760e92549614cf47bc9a732eb58b7864ecfe38
yjyj1023 <lyj19961023@gmail.com>
2023-06-01 15:23:54 +0900

    [Update] 시큐리티 설정 변경

--- src/main/java/com/metanet/metabus/config/SecurityConfig.java
+++ src/main/java/com/metanet/metabus/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.metanet.metabus.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,13 +15,13 @@ public class SecurityConfig {
 
         http.cors();
 
-        http.authorizeRequests()
-                .antMatchers("/member/register", "/member/login").permitAll()
-//                .antMatchers("/member/mypage").hasAnyRole("ROLE_USER", "ROLE_ADMIN")
-                .antMatchers(HttpMethod.GET, "/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/**").authenticated()
-                .antMatchers(HttpMethod.PUT, "/**").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/**").authenticated();
+//        http.authorizeRequests()
+//                .antMatchers("/member/register", "/member/login").permitAll()
+////                .antMatchers("/member/mypage").hasAnyRole("ROLE_USER", "ROLE_ADMIN")
+//                .antMatchers(HttpMethod.GET, "/**").permitAll()
+//                .antMatchers(HttpMethod.POST, "/**").authenticated()
+//                .antMatchers(HttpMethod.PUT, "/**").authenticated()
+//                .antMatchers(HttpMethod.DELETE, "/**").authenticated();
 
         return http.build();
     }
