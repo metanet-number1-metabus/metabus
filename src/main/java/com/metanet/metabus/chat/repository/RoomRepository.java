bf4bc5c897d259135858b940208031bee6d6dae4
root <root@metanet.pri>
2023-06-01 10:49:55 +0900

    [Feat] chat 관련 기능 로직 수정 및 아이콘 표시 수정 METABUS-17

--- src/main/java/com/metanet/metabus/chat/repository/RoomRepository.java
+++ src/main/java/com/metanet/metabus/chat/repository/RoomRepository.java
@@ -2,7 +2,11 @@ package com.metanet.metabus.chat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.metanet.metabus.chat.entity.Room;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-
+    @Query("SELECT r from Room r where r.memId = :id")
+    List<Room> findUser(Long id);
 }
