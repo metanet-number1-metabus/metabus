53f50a3c6a47d53cfd49749556d07fdb16d559c8
yjyj1023 <lyj19961023@gmail.com>
2023-06-02 16:48:43 +0900

    [Update] Member UserDetails 삭제

--- src/main/java/com/metanet/metabus/member/entity/Member.java
+++ src/main/java/com/metanet/metabus/member/entity/Member.java
@@ -5,21 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Table
-public class Member extends BaseEntity implements UserDetails {
+public class Member extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -54,45 +48,4 @@ public class Member extends BaseEntity implements UserDetails {
         this.role = role == null ? Role.USER : role;
         this.phoneNum = phoneNum;
     }
-
-    //권한 리턴
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<GrantedAuthority>(List.of(new SimpleGrantedAuthority(role.name())));
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.email;
-    }
-
-    // 계정 만료 여부 반환
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    // 계정 잠금 여부 반환
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    // 패스워드의 만료 여부 반환
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    // 계정 활성화 여부 반환
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-
 }
