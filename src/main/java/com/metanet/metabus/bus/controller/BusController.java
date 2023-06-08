29901a292ed232893d4887795276b7c874536269
kimgunbin <rjsqls456@naver.com>
2023-06-08 09:31:15 +0900

    [Feat] bus reservation 데이터 종류별 분류 METABUS-33

--- src/main/java/com/metanet/metabus/bus/controller/BusController.java
+++ src/main/java/com/metanet/metabus/bus/controller/BusController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -37,7 +38,8 @@ public class BusController {
     }
 
     @GetMapping("/bus/reservation")
-    public String readReservation(HttpSession session, Model model) {
+    public String readReservation(HttpServletRequest request, HttpSession session, Model model) {
+        String specialValue = request.getParameter("specialValue");
 
         MemberDto memberDto = (MemberDto) session.getAttribute("loginMember");
 
@@ -46,10 +48,25 @@ public class BusController {
         List<Reservation> paidReservationList = reservationService.readPaidReservation(memberDto);
         List<Reservation> pastReservationList = reservationService.readPastReservation(memberDto);
 
-        model.addAttribute("allReservationList", allReservationList);
-        model.addAttribute("unpaidReservationList", unpaidReservationList);
-        model.addAttribute("paidReservationList", paidReservationList);
-        model.addAttribute("pastReservationList", pastReservationList);
+        if(specialValue==null){
+            model.addAttribute("ReservationList", allReservationList);
+            model.addAttribute("checkButton","전체");
+        }else if(specialValue.equals("전체")){
+            model.addAttribute("ReservationList", allReservationList);
+            model.addAttribute("checkButton","전체");
+        }else if(specialValue.equals("결제대기")){
+            model.addAttribute("ReservationList", unpaidReservationList);
+            model.addAttribute("checkButton","결제대기");
+        }else if(specialValue.equals("결제완료")){
+            model.addAttribute("ReservationList", paidReservationList);
+            model.addAttribute("checkButton","결제완료");
+        } else if (specialValue.equals("만료된 승차권")) {
+            model.addAttribute("ReservationList", pastReservationList);
+            model.addAttribute("checkButton","만료된 승차권");
+        }
+
+
+
 
         return "bus/bus-reservation-table";
     }
