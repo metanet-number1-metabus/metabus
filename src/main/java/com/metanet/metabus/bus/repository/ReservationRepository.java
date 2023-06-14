cbf357a358acf7065b520f9f8df54a36869fd6d8
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-06-14 16:54:30 +0900

    [Update] 예약 리포 수
    METABUS-50

--- src/main/java/com/metanet/metabus/bus/repository/ReservationRepository.java
+++ src/main/java/com/metanet/metabus/bus/repository/ReservationRepository.java
@@ -1,11 +1,10 @@
 package com.metanet.metabus.bus.repository;
 
-import com.metanet.metabus.bus.entity.PaymentStatus;
 import com.metanet.metabus.bus.entity.Reservation;
+import com.metanet.metabus.bus.entity.ReservationStatus;
 import com.metanet.metabus.bus.entity.Seat;
 import com.metanet.metabus.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,20 +13,13 @@ import java.util.List;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findByMemberAndDeletedDateIsNullOrderByDepartureDateDesc(Member member);
 
-    @Query("SELECT r FROM Reservation r WHERE r.member = :member AND r.deletedDate IS NULL AND r.paymentStatus = 'UNPAID' AND r.departureDate >= :today")
-    List<Reservation> findUnpaidReservationsByMemberOrderByDepartureDateDesc(Member member, LocalDate today);
-
-    @Query("SELECT r FROM Reservation r WHERE r.member = :member AND r.deletedDate IS NULL AND r.paymentStatus = 'PAID' AND r.departureDate >= :today")
-    List<Reservation> findPaidReservationsByMemberOrderByDepartureDateDesc(Member member, LocalDate today);
-
-    @Query("SELECT r FROM Reservation r WHERE r.member = :member AND r.deletedDate IS NULL AND r.departureDate < :today")
-    List<Reservation> findPastReservationsByMemberOrderByDepartureDateDesc(Member member, LocalDate today);
+    List<Reservation> findByMemberAndDeletedDateIsNullAndReservationStatusOrderByDepartureDateDesc(Member member, ReservationStatus reservationStatus);
 
     Reservation findByIdAndDeletedDateIsNull(Long reservationId);
 
-    List<Reservation> findByCreatedDateBeforeAndPaymentStatusAndDeletedDateIsNull(LocalDateTime threshold, PaymentStatus paymentStatus);
+    List<Reservation> findByCreatedDateBeforeAndReservationStatusAndDeletedDateIsNull(LocalDateTime threshold, ReservationStatus reservationStatus);
 
     Reservation findBySeatId(Seat seat);
 
-    List<Reservation> findByDepartureDateAndDeletedDateIsNullAndPaymentStatus(LocalDate yesterday, PaymentStatus paymentStatus);
+    List<Reservation> findByDepartureDateAndDeletedDateIsNullAndReservationStatus(LocalDate yesterday, ReservationStatus reservationStatus);
 }
