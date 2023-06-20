ae3220c88d47a5e5faf59b3a7c1742d5371d0d31
yjyj1023 <lyj19961023@gmail.com>
2023-06-20 15:45:37 +0900

    [Refactor] 이메일 valid 수정

--- src/main/java/com/metanet/metabus/member/dto/MemberLoginRequest.java
+++ src/main/java/com/metanet/metabus/member/dto/MemberLoginRequest.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -17,6 +16,5 @@ public class MemberLoginRequest {
     private String email;
 
     @NotBlank(message = "패스워드를 입력해주세요.")
-    @Pattern(regexp = "(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}", message = "영문 숫자 조합 8자리 이상이여야 합니다.")
     private String password;
 }
